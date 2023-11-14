0550bdb2a7be21dd91485ad367879459a47a2c83
lavskillup <lavanyas@skillup.tech>
2023-11-14 16:54:52 +1100

    Added initial files

---
+++ server/djangoapp/urls.py
@@ -0,0 +1,27 @@
+from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
+from . import views
+
+app_name = 'djangoapp'
+urlpatterns = [
+    # route is a string contains a URL pattern
+    # view refers to the view function
+    # name the URL
+
+    # path for about view
+
+    # path for contact us view
+
+    # path for registration
+
+    # path for login
+
+    #path to landing page and home
+    path(route='', view=views.home, name='index'),
+
+    # path for dealer reviews view
+
+    # path for add a review view
+
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
