381441aa5bf6734c0ffebde0d9c648c16fd97d23
lavskillup <73978832+lavskillup@users.noreply.github.com>
2023-11-19 20:44:03 +1100

    Update settings.py

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -31,9 +31,7 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
-    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [],
 }
 
 # Application definition
