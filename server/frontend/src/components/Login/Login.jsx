f33cac599b7554ac2868b2f523c84891fe7c56f4
lavskillup <73978832+lavskillup@users.noreply.github.com>
2023-11-19 22:07:37 +1100

    Update Login.jsx

--- server/frontend/src/components/Login/Login.jsx
+++ server/frontend/src/components/Login/Login.jsx
@@ -1,8 +1,5 @@
 import React, { useState } from 'react';
-// import { Link, useNavigate } from 'react-router-dom';
 import "./Login.css";
-
-
 
 const Login = () => {
   const [userName, setUserName] = useState("");
