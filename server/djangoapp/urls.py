d607fbac55fb878d9ea62be78f78f068cf2977a8
lavskillup <73978832+lavskillup@users.noreply.github.com>
2023-11-21 07:07:31 +1100

    Update urls.py

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -6,18 +6,13 @@ from . import views
 app_name = 'djangoapp'
 urlpatterns = [
     # # path for about view
-    # path(route='about/', view=views.about, name='about'),
 
     # # path for contact us view
-    # path(route='contact/', view=views.contact, name='contact'),
 
     # # path for registration
-    # path(route='registration/', view=views.registration, name='registration'),
 
     # path for login
-    path(route='login', view=views.login_user, name='login'),
-    path('register', views.registration, name='register'),
-    path('logout', views.logout_request, name='logout'),
+    # path(route='login', view=views.login_user, name='login'),
 
     # #path to landing page and home
     # path(route='', view=views.home, name='home'),
