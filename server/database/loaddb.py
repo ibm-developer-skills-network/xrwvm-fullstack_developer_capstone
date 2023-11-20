e96741389386a350ed534fe3fc2cd3b18d67c70f
lavskillup <73978832+lavskillup@users.noreply.github.com>
2023-11-20 19:12:53 +1100

    Create loaddb.py

---
+++ server/database/loaddb.py
@@ -0,0 +1,44 @@
+import json
+from pymongo import MongoClient
+
+# MongoDB connection details
+mongo_uri = "mongodb://root:password@localhost:27017/"
+
+# Connect to MongoDB with authentication
+client = MongoClient(mongo_uri)
+database = client["dealershipsdb"]
+
+collection = database["dealerships"]
+
+# Load data from JSON file
+json_file = "dealership.json"
+
+with open(json_file, 'r') as file:
+    data = json.load(file)
+
+collection.insert_many(data)
+
+collection = database["reviews]
+
+# Load data from JSON file
+json_file = "reviews.json"
+
+with open(json_file, 'r') as file:
+    data = json.load(file)
+
+collection.insert_many(data)
+
+collection = database["reviews-full"]
+
+# Load data from JSON file
+json_file = "reviews-full.json"
+
+with open(json_file, 'r') as file:
+    data = json.load(file)
+
+collection.insert_many(data)
+
+# Close MongoDB connection
+client.close()
+
+print("Data loaded into MongoDB successfully")
