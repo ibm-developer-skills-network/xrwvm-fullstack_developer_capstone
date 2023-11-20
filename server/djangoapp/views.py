40180bf341a6e9776b1ee942dc1c7f1a53a9b806
lavskillup <73978832+lavskillup@users.noreply.github.com>
2023-11-20 13:04:23 +1100

    Update views.py

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -35,51 +35,17 @@ def login_user(request):
     return JsonResponse(data)
 
 # Create a `logout_request` view to handle sign out request
-def logout_request(request):
-    logout(request)
-    data = {"userName":""}
-    return JsonResponse(data)
+# def logout_request(request):
+# ...
 
 # Create a `registration` view to handle sign up request
-@csrf_exempt
-def registration(request):
-    context = {}
-
-    data = json.loads(request.body)
-    username = data['userName']
-    password = data['password']
-    first_name = data['firstName']
-    last_name = data['lastName']
-    email = data['email']
-    username_exist = False
-    email_exist = False
-    try:
-        # Check if user already exists
-        User.objects.get(username=username)
-        username_exist = True
-    except:
-        # If not, simply log this is a new user
-        logger.debug("{} is new user".format(username))
-
-    # If it is a new user
-    if not username_exist:
-        # Create user in auth_user table
-        user = User.objects.create_user(username=username, first_name=first_name, last_name=last_name,
-                                        password=password, email=email)
-        # Login the user and redirect to list page
-        login(request, user)
-        data = {"userName":username,"status":"Authenticated"}
-        return JsonResponse(data)
-    else :
-        data = {"userName":username,"error":"Already Registered"}
-        return JsonResponse(data)
-    
+#@csrf_exempt
+#def registration(request):
+# ...
 
 # # Update the `get_dealerships` view to render the index page with a list of dealerships
 # def get_dealerships(request):
-#     context = {}
-#     if request.method == "GET":
-#         return render(request, 'djangoapp/index.html', context)
+# ...
 
 
 # Create a `get_dealer_details` view to render the reviews of a dealer
