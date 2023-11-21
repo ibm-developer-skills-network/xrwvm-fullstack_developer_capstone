acf35ca4f94339fa9d4f77a6c52dad975830ea36
lavskillup <73978832+lavskillup@users.noreply.github.com>
2023-11-21 13:00:41 +1100

    Create dealership.js

---
+++ server/database/dealership.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const dealerships = new Schema({
+	id: {
+    type: Number,
+    required: true,
+	},
+	city: {
+    type: String,
+    required: true
+  },
+  state: {
+    type: String,
+    required: true
+  },
+  address: {
+    type: String,
+    required: true
+  },
+  zip: {
+    type: String,
+    required: true
+  },
+  lat: {
+    type: String,
+    required: true
+  },
+  long: {
+    type: String,
+    required: true
+  },
+  short_name: {
+    type: String,
+  },
+  full_name: {
+    type: String,
+    required: true
+  }
+});
+
+module.exports = mongoose.model('dealerships', dealerships);
