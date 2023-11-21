709a4adc806aba74365a568e5be567a3835e9766
lavskillup <73978832+lavskillup@users.noreply.github.com>
2023-11-21 14:41:59 +1100

    Update app.js

--- server/database/app.js
+++ server/database/app.js
@@ -20,6 +20,48 @@ const Dealerships = require('./dealership');
 Reviews.insertMany(reviews_data['reviews']);
 Dealerships.insertMany(dealerships_data['dealerships'])
 
+
+// Express route to fetch all reviews
+app.get('/fetchReviews', async (req, res) => {
+  try {
+    const documents = await Reviews.find();
+    res.json(documents);
+  } catch (error) {
+    res.status(500).json({ error: 'Error fetching documents' });
+  }
+});
+
+// Express route to fetch all dealerships
+app.get('/fetchDealers', async (req, res) => {
+	//Write the code here
+});
+
+// Express route to fetch reviews by a particular user
+app.get('/fetch/:username', async (req, res) => {
+	//Write the code here
+});
+
+// Express route to fetch reviews by a particular car name
+app.get('/fetchReviews/car/:carmake', async (req, res) => {
+  try {
+    const documents = await Reviews.find({car_make: req.params.carmake});
+    res.json(documents);
+  } catch (error) {
+    res.status(500).json({ error: 'Error fetching documents' });
+  }
+});
+
+// Express route to fetch reviews by a particular car name
+app.get('/fetchRevies/car/:carmake/:model', async (req, res) => {
+  try {
+    const documents = await Reviews.find({car_make: req.params.carmake, car_model: req.params.carmodel});
+    res.json(documents);
+  } catch (error) {
+    res.status(500).json({ error: 'Error fetching documents' });
+  }
+});
+
+//Express route to insert review
 app.get('/insert', async (req, res) => {
   const review = new Reviews({
 		"id": req.query['id'],
@@ -39,16 +81,6 @@ app.get('/insert', async (req, res) => {
   } catch (error) {
 		console.log(error);
     res.status(500).json({ error: 'Error inserting review' });
-  }
-});
-
-// Express route to fetch all documents
-app.get('/fetch', async (req, res) => {
-  try {
-    const documents = await Reviews.find();
-    res.json(documents);
-  } catch (error) {
-    res.status(500).json({ error: 'Error fetching documents' });
   }
 });
 
