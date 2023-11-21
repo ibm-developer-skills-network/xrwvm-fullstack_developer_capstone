ed07f92cdd702fd9dec0dc900524a7ccc778889f
lavskillup <73978832+lavskillup@users.noreply.github.com>
2023-11-21 13:00:06 +1100

    Create review.js

---
+++ server/database/review.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const reviews = new Schema({
+	id: {
+    type: Number,
+    required: true,
+	},
+	name: {
+    type: String,
+    required: true
+  },
+  dealership: {
+    type: Number,
+    required: true,
+  },
+  review: {
+    type: String,
+    required: true
+  },
+  purchase: {
+    type: Boolean,
+    required: true
+  },
+  purchase_date: {
+    type: String,
+    required: true
+  },
+  car_make: {
+    type: String,
+    required: true
+  },
+  car_model: {
+    type: String,
+    required: true
+  },
+  car_year: {
+    type: Number,
+    required: true
+  },
+});
+
+module.exports = mongoose.model('reviews', reviews);
