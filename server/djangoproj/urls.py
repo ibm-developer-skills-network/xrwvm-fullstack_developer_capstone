5e99286e936f6981007d4702fa5278fab68c1dad
lavskillup <73978832+lavskillup@users.noreply.github.com>
2023-11-22 21:32:24 +1100

    Update urls.py

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -20,4 +20,5 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('djangoapp/', include('djangoapp.urls')),
+    path('', TemplateView.as_view(template_name="index.html")),
 ]
