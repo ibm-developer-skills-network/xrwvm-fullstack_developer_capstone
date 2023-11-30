66fcf3acdabfcd833ca481b3365d73b7a95ddbbb
lavskillup <73978832+lavskillup@users.noreply.github.com>
2023-11-30 17:47:32 +1100

    Create Dealer.jsx

---
+++ server/frontend/src/components/Dealers/Dealer.jsx
@@ -0,0 +1,94 @@
+import React, { useState,useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import "./Dealers.css";
+import "../assets/style.css";
+import positive_icon from "../assets/positive.png"
+import neutral_icon from "../assets/neutral.png"
+import negative_icon from "../assets/negative.png"
+
+const Dealer = () => {
+
+
+  const [dealer, setDealer] = useState({});
+  const [reviews, setReviews] = useState([]);
+  const [unreviewed, setUnreviewed] = useState(false);
+  const [postReview, setPostReview] = useState(<></>)
+
+  let curr_url = window.location.href;
+  let root_url = curr_url.substring(0,curr_url.indexOf("dealer"));
+  let params = useParams();
+  let id =params.id;
+  let dealer_url = root_url+`djangoapp/dealer/${id}`;
+  let reviews_url = root_url+`djangoapp/reviews/dealer/${id}`;
+  let post_review = root_url+`postreview/${id}`;
+  
+  const get_dealer = async ()=>{
+    const res = await fetch(dealer_url, {
+      method: "GET"
+    });
+    const retobj = await res.json();
+    
+    if(retobj.status === 200) {
+      let dealerobjs = Array.from(retobj.dealer)
+      setDealer(dealerobjs[0])
+    }
+  }
+
+  const get_reviews = async ()=>{
+    const res = await fetch(reviews_url, {
+      method: "GET"
+    });
+    const retobj = await res.json();
+    
+    if(retobj.status === 200) {
+      if(retobj.reviews.length > 0){
+        setReviews(retobj.reviews)
+      } else {
+        setUnreviewed(true);
+      }
+    }
+  }
+
+  const senti_icon = (sentiment)=>{
+    let icon = sentiment === "positive"?positive_icon:sentiment==="negative"?negative_icon:neutral_icon;
+    return icon;
+  }
+
+  useEffect(() => {
+    get_dealer();
+    get_reviews();
+    if(sessionStorage.getItem("username")) {
+      setPostReview(<a href={post_review}>Post Review</a>)
+    }
+  },[]);  
+
+
+return(
+  <div>
+    <div className="navcontainer">
+
+      <div className='navitems'>
+      <text className="small_header">Dealership Reviews</text>
+
+        <a className="nav_item" href="/">Home</a>
+        <a className="nav_item" href="/about">About Us</a>
+        <a className="nav_item" href="/contact">Contact Us</a>
+      </div>
+    </div>
+      <h1>{dealer.full_name}</h1>
+      {postReview}
+      <div>
+      {unreviewed ? (
+        <text>No Reviews Yet</text>
+      ):reviews.map(review => (
+        <div className='review'>
+        <img src={senti_icon(review.sentiment)} className="img_icon" alt='Username'/>
+        <p>{review.review}</p>
+        </div>
+      ))}
+    </div>  
+  </div>
+)
+}
+
+export default Dealer
