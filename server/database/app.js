cc84cc31d2b12fbf2a09840ad8f59d25c0bb1818
lavskillup <73978832+lavskillup@users.noreply.github.com>
2023-11-30 16:22:47 +1100

    Update app.js

--- server/database/app.js
+++ server/database/app.js
@@ -48,22 +48,12 @@ app.get('/fetchReviews', async (req, res) => {
 
 // Express route to fetch all dealerships
 app.get('/fetchDealers', async (req, res) => {
-  try {
-    const documents = await Dealerships.find();
-    res.json(documents);
-  } catch (error) {
-    res.status(500).json({ error: 'Error fetching documents' });
-  }
+	//Write your code here
 });
 
 // Express route to fetch reviews by a particular user
 app.get('/fetchReviews/:username', async (req, res) => {
-  try {
-    const documents = await Reviews.find({name: req.params.username});
-    res.json(documents);
-  } catch (error) {
-    res.status(500).json({ error: 'Error fetching documents' });
-  }
+	//Write your code here
 });
 
 // Express route to fetch reviews by a particular user
