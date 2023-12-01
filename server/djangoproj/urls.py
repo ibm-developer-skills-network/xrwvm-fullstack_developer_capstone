89d123f6066e15ba800812fdaac7ea688045e093
lavskillup <73978832+lavskillup@users.noreply.github.com>
2023-12-02 07:03:40 +1100

    Update urls.py

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('djangoapp/', include('djangoapp.urls')),
     path('', TemplateView.as_view(template_name="index.html")),
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
