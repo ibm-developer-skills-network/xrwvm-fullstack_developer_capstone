07f2f95a9870caebc1e48631485dbf2c0439add7
lavskillup <73978832+lavskillup@users.noreply.github.com>
2023-12-05 10:39:35 +1100

    Delete server/entrypoint.sh

--- server/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# Make migrations and migrate the database.
-echo "Making migrations and migrating the database. "
-python manage.py makemigrations --noinput
-python manage.py migrate --noinput
-python manage.py collectstatic --noinput
-exec "$@"
