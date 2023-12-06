30584dcdeff178d33d7fdafb5ebce76433e83bb8
Lavanya T S <43871747+lauvshree@users.noreply.github.com>
2023-12-06 12:02:05 +1100

    Update app.py

--- server/djangoapp/microservices/app.py
+++ server/djangoapp/microservices/app.py
@@ -8,7 +8,8 @@ sia = SentimentIntensityAnalyzer()
 
 @app.get('/')
 def home():
-    return "Welcome to the Sentiment Analyzer. Use /analyze/text to get the sentiment"
+    return "Welcome to the Sentiment Analyzer. \
+    Use /analyze/text to get the sentiment"
 
 
 @app.get('/analyze/<input_txt>')
@@ -20,7 +21,7 @@ def analyze_sentiment(input_txt):
     neg = float(scores['neg'])
     neu = float(scores['neu'])
     res = "positive"
-    print("pos neg nue ",pos,neg,neu)    
+    print("pos neg nue ", pos, neg, neu)    
     if(neg>pos and neg>neu):
         res = "negative"
     elif(neu>neg and neu>pos):
@@ -31,4 +32,4 @@ def analyze_sentiment(input_txt):
     
 
 if __name__=="__main__":
-    app.run(debug=True) 
+    app.run(debug=True)
