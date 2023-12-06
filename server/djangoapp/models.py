e2288952875c3e307086ee9a4f142dbb8dfd1c98
Lavanya T S <43871747+lauvshree@users.noreply.github.com>
2023-12-06 12:07:35 +1100

    Update models.py

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,6 +1,8 @@
-from django.db import models
-from django.utils.timezone import now
-from django.core.validators import MaxValueValidator, MinValueValidator
+# Uncomment the following imports before adding the Model code
+
+# from django.db import models
+# from django.utils.timezone import now
+# from django.core.validators import MaxValueValidator, MinValueValidator
 
 
 # Create your models here.
