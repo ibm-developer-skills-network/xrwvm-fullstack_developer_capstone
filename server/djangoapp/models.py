8d9bef72e5f888a77ad412ed4a8e774acac2cba6
Lavanya T S <43871747+lauvshree@users.noreply.github.com>
2023-12-06 12:11:47 +1100

    Update models.py

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -15,9 +15,11 @@
 
 
 # <HINT> Create a Car Model model `class CarModel(models.Model):`:
-# - Many-To-One relationship to Car Make model (One Car Make has many Car Models, using ForeignKey field)
+# - Many-To-One relationship to Car Make model (One Car Make has many 
+# Car Models, using ForeignKey field)
 # - Name
-# - Type (CharField with a choices argument to provide limited choices such as Sedan, SUV, WAGON, etc.)
+# - Type (CharField with a choices argument to provide limited choices 
+# such as Sedan, SUV, WAGON, etc.)
 # - Year (IntegerField) with min value 2015 and max value 2023
 # - Any other fields you would like to include in car model
 # - __str__ method to print a car make object
