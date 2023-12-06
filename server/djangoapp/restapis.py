e507c9d901ccd6d67a729b3eb7eab62e58b3c885
Lavanya T S <43871747+lauvshree@users.noreply.github.com>
2023-12-06 12:13:28 +1100

    Update restapis.py

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -1,6 +1,7 @@
-import requests
-import json
-from requests.auth import HTTPBasicAuth
+# Uncomment the imports below before you add the function code
+# import requests
+# import json
+# from requests.auth import HTTPBasicAuth
 
 import os
 from dotenv import load_dotenv
