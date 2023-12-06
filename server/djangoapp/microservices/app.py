bdee3664aa3f0965543e25d3ffedd6f825348d53
Lavanya T S <43871747+lauvshree@users.noreply.github.com>
2023-12-06 12:10:41 +1100

    Update app.py

--- server/djangoapp/microservices/app.py
+++ server/djangoapp/microservices/app.py
@@ -21,15 +21,15 @@ def analyze_sentiment(input_txt):
     neg = float(scores['neg'])
     neu = float(scores['neu'])
     res = "positive"
-    print("pos neg nue ", pos, neg, neu)  
+    print("pos neg nue ", pos, neg, neu)
     if (neg > pos and neg > neu):
         res = "negative"
     elif (neu > neg and neu > pos):
         res = "neutral"
-    res = json.dumps({"sentiment":res})
+    res = json.dumps({"sentiment : res})
     print(res)
     return res
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     app.run(debug=True)
