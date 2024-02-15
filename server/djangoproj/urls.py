3857d2d60ad70ba78f79facf14e81d045ad7aa4d
KheprJr <matthewsben91@gmail.com>
2024-02-15 15:34:02 -0500

    help

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -27,4 +27,6 @@ urlpatterns = [
     path('register/', TemplateView.as_view(template_name="index.html")),
     path('djangoapp/', include('djangoapp.urls')),
     path('', TemplateView.as_view(template_name="Home.html")),
+    path('dealers/', TemplateView.as_view(template_name="index.html")),
+
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,12 +1,14 @@
 import LoginPanel from "./components/Login/Login"
 import Register from "./components/Register/Register"
 import { Routes, Route } from "react-router-dom";
+import Dealers from './components/Dealers/Dealers';
 
 function App() {
   return (
     <Routes>
       <Route path="/login" element={<LoginPanel />} />
       <Route path="/register" element={<Register />} />
+      <Route path="/dealers" element={<Dealers/>} />
     </Routes>
   );
 }
