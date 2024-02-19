57a6bf72d6d5214a860ec7bd68743a5a17ce08fb
KhepriJr <matthewsben91@gmail.com>
2024-02-19 11:26:27 -0500

    lint 3

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,8 +1,7 @@
 # Uncomment the following imports before adding the Model code
 from django.db import models
 #from django.utils.timezone import now
-from django.core.validators 
-import MaxValueValidator, MinValueValidator
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 
 # Create your models here.
--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -11,13 +11,14 @@ backend_url = os.getenv(
     'backend_url', default="http://localhost:3030")
 sentiment_analyzer_url = os.getenv(
     'sentiment_analyzer_url',
-    default = "http://localhost:5050/")
+    default="http://localhost:5050/")
+
 
 def get_request(endpoint, **kwargs):
     params = ""
     if(kwargs):
-        for key,value in kwargs.items():
-            params= params+key + "=" + value + "&"
+        for key, value in kwargs.items():
+            params = params+key + "=" + value + "&"
 
     request_url = backend_url + endpoint + "?" + params
 
