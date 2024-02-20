4bf702a29e1ee5d8bab7be4cfc0c724c5cd5cff7
Binay Humagain <humagain.binay@gmail.com>
2024-02-20 15:42:18 -0500

    Temporary changes

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -22,5 +22,7 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('djangoapp/', include('djangoapp.urls')),
+    path('about/', TemplateView.as_view(template_name="About.html")),
+    path('contact/', TemplateView.as_view(template_name="contact.html")),
     path('', TemplateView.as_view(template_name="Home.html")),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
