432cca1e0eadaf53c8937c0f1299bdc963327a21
Binay Humagain <humagain.binay@gmail.com>
2024-02-22 01:07:59 -0500

    React push

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -22,7 +22,9 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('djangoapp/', include('djangoapp.urls')),
-    path('about/', TemplateView.as_view(template_name="About.html")),
+    path('login/', TemplateView.as_view(template_name="index.html")),
+    path('register/', TemplateView.as_view(template_name="index.html")),
+    path('about/', TemplateView.as_view(template_name="About.html")),    
     path('contact/', TemplateView.as_view(template_name="contact.html")),
     path('', TemplateView.as_view(template_name="Home.html")),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
