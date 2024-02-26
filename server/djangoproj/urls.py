dc539d9ef5a888b2e8b41accd2917b8732c24250
miguelcasadinho <miguel.casadinho@gmail.com>
2024-02-26 05:20:14 -0500

    Adding temporary changes

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('djangoapp/', include('djangoapp.urls')),
     path('', TemplateView.as_view(template_name="Home.html")),
+    path('about/', TemplateView.as_view(template_name="About.html")),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
